bf4e6e4f80d52afac03e52c24a3247f054b244bf
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-28 13:44:24 +0430

    Subject: main.tsx, Description: Add routes, Date: 7/28/2023

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import routes from './routes/routes'
+import "./main.css"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={routes} />
   </React.StrictMode>,
 )
