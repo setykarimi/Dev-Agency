cbc7e97a9f6dcc540a4dc0b8445d529e90f97940
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-28 13:40:38 +0430

    Subject: Config, Description: Defaine alias for importing component, Date: 7/28/2023

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      Components: "/src/components",
+      Pages: "/src/pages",
+      Layout: "/src/layout",
+    },
+  },
 })
