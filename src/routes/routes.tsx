85534c62d1f87f1c89ea995e9d0dde96f7c22ae3
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-29 21:24:41 +0430

    Subject: Layout, Description: Add Layout for pages, Date: 7/29/2023

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from 'Pages/home'
+import Layout from 'Layout/layout';
 
 const routes = createBrowserRouter([
   {
     path: "/",
-    element: <Home /> 
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      }
+    ]
   },
 ]);
 
