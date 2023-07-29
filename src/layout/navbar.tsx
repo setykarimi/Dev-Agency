f709d9ed752f6dce45f409216271d8f80f1eb2e1
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-29 21:25:03 +0430

    Subject: Navbar, Description: Edit navbar content, Date: 7/29/2023

--- src/layout/navbar.tsx
+++ src/layout/navbar.tsx
@@ -1,7 +1,45 @@
+import logo from 'Images/logo.svg'
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
-    return ( <div>
-        Navbar
-    </div> );
+    const menuItems = [
+        {
+            name: "Home",
+            link: "/"
+        },
+        {
+            name: "Our Services",
+            link: "/our-services"
+        },
+        {
+            name: "Projects",
+            link: "/projects"
+        },
+        {
+            name: "Blog",
+            link: "/blog"
+        },
+        {
+            name: "About",
+            link: "/about"
+        }
+    ]
+    return (
+        <nav className="bg-background-merino p-4 flex">
+            <div className="container md:px-8 px-4 mx-auto flex justify-between items-center">
+                <div className="logo flex items-center gap-2">
+                    <img src={logo} alt='logo' className='w-4' />
+                    <span className='font-bold text-typography-asphalt text-xl'>DevAgency</span>
+                </div>
+                <div className='md:flex hidden gap-8 items-center'>
+                    <div className='flex gap-4'>
+                        {menuItems.map(({ name, link }) => <Link to={link} className='font-medium border-b border-b-transparent text-typography-flint hover:border-b hover:border-b-primary-orange-default hover:font-bold transition-all'>{name}</Link>)}
+                    </div>
+                    <button className='py-2 px-4 bg-primary-orange-default text-white rounded'>Contact</button>
+                </div>
+            </div>
+        </nav>
+    );
 }
- 
+
 export default Navbar;
