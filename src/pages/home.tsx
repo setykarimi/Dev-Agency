5f351720b677f9c0d31471d22b7ed9a90083aa82
setykarimi <setykarimi80@gmail.com>
2023-07-30 09:06:15 +0330

    Subject: Homepage, Description: Edit heroSection for responsive view, Date: 1402/05/08

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,7 @@ const Home = () => {
     <div className="bg-background-merino p-4">
       <div className="container mx-auto">
         <div className="flex flex-col items-center justify-center pb-8">
-          <h1 className="flex flex-col text-5xl font-extra-bold items-center mt-8">
+          <h1 className="flex flex-col md:text-5xl text-2xl font-extra-bold items-center md:mt-8 mt-4">
             <span className="flex gap-2 items-center">
               {" "}
               We Build <span className="p-1 bg-background-maize">
@@ -15,14 +15,14 @@ const Home = () => {
             <span className="block mt-2">For Your Success</span>
           </h1>
           <div className="mt-6 flex gap-4">
-            <button className="text-white py-3 px-8 rounded bg-primary-orange-default text-lg font-medium">
+            <button className="text-white md:py-3 py-2 md:px-8 px-4 rounded bg-primary-orange-default md:text-xl font-medium">
                 Get Started
             </button>
-            <button className="text-primary-orange-default py-3 px-8 rounded bg-background-maize text-lg font-medium">
+            <button className="text-primary-orange-default md:py-3 py-2 md:px-8 px-4 rounded bg-background-maize md:text-lg font-medium">
                 Watch Demo
             </button>
           </div>
-          <img src={homeHeroSection} className="-mt-44"/>
+          <img src={homeHeroSection} className="md:-mt-44 -mt-12"/>
         </div>
       </div>
     </div>
