040327d31cee39ed92addbfb52036859f40ab898
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-31 23:00:22 +0430

    Subject: Cards, Description: Add card, Date: 7/31/2023

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,15 @@
 import Brands from "Components/brands/brands";
 import BannerHome from "Components/banners/bannerHome";
+import CardOurProjects from "Components/cards/cardOurProjects";
 
 const Home = () => {
   return (
-   <>
-   <BannerHome />
-    <Brands />
+    <>
+      <BannerHome />
+      <div className="container md:px-12 px-4 mx-auto ">
+        <Brands />
+        <CardOurProjects />
+      </div>
     </>
   );
 };
