771109df8e21345d95506ba316d11741bbab2025
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-31 22:59:09 +0430

    Subject: Cards, Description: Create new folder ofr cards, Date: 7/31/2023

---
+++ src/components/cards/cardOurProjects.tsx
@@ -0,0 +1,42 @@
+import Title from "Components/title/title";
+
+import img1 from "Images/cards/productLandingpage1.png";
+import img2 from "Images/cards/productLandingpage2.png";
+
+const CardOurProjects = () => {
+    return (
+        <div className="mt-12">
+           <div className="grid md:grid-cols-2 grid-cols-1 gap-8 mt-8 items-end">
+                <div>
+                    <Title title="Our Projects" />
+                    <p className="text-typography-flint text-lg mt-2">
+                        <span className="block">Praesent placerated egestas egestas cras rhone</span>
+                        magna rutrum tellus laoreet
+                    </p>
+                </div>
+
+              <div className="flex md:justify-end justify-center md:order-none order-last ">
+              <button className="bg-background-merino text-primary-orange-default md:py-3 py-2 md:px-8 px-4 h-fit w-fit rounded font-medium">See More</button>
+              </div>
+            
+           
+                <div className="bg-[#F5E6FE] pt-12 px-4 rounded">
+                    <div className="block text-2xl font-bold text-center">
+                        <span className="block">Watch - Product Responsive </span>
+                        <span className="block">Landing Page</span>
+                    </div>
+                    <img src={img1} alt="landing" className="mt-8 mx-auto"/>
+                </div>
+                <div className="bg-background-merino pt-12 px-4 rounded">
+                <div className="block text-2xl font-bold text-center">
+                        <span className="block">SEO Agency - Creative</span>
+                        <span className="block">Landing Page</span>
+                    </div>
+                    <img src={img2} alt="landing" className="mt-8 mx-auto"/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CardOurProjects;
