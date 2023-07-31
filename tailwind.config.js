dac67543f5a1c04c327b1fb641ec2a5c98b2c361
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-31 22:59:54 +0430

    Subject: Config, Description: Add z-index, Date: 7/31/2023

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      zIndex: {
+        "-1": "-1"
+      }
+    },
     colors: {
       primary: {
         orange: {
