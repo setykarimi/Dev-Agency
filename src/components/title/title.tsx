e1df4bb18ed060b10e8a1f92193cb5853e7ee32d
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-08-05 21:37:21 +0330

    Subject: Style, Description: Edit styles for responsive view, Date: 8/5/2023

--- src/components/title/title.tsx
+++ src/components/title/title.tsx
@@ -2,7 +2,7 @@ type TitleProps = {
     title: StringConstructor
 }
 const Title = ({title}: TitleProps) => {
-    return ( <h3 className="w-fit relative font-bold text-4xl after:h-2 after:bg-background-maize after:w-full after:block after:absolute after:bottom-2 after:-z-1">
+    return ( <h3 className="w-fit relative font-bold md:text-4xl text-2xl after:h-2 after:bg-background-maize after:w-full after:block after:absolute after:bottom-2 after:-z-1">
         {title}
     </h3> );
 }
