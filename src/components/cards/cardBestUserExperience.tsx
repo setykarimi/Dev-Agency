5330417dc2534fa812bca8893105c4361f7061d0
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-08-06 21:39:21 +0330

    Subject: cardBestUserExperience, Description: Edit content, Date: 8/6/2023

--- src/components/cards/cardBestUserExperience.tsx
+++ src/components/cards/cardBestUserExperience.tsx
@@ -1,27 +1,35 @@
 import Title from "Components/title/title";
 import check from 'Images/icons/check.svg';
+import img from "Images/bestUserExperience.png";
+import bgImg from "Images/bestUserExperienceBg.png";
 
 const CardBestUserExperience = () => {
     const items = [
-        "Blandit ut nunc vel bibendum semper elit","Bibendum blandit ut nunc vel "
+        "Blandit ut nunc vel bibendum semper elit", "Bibendum blandit ut nunc vel "
     ]
-    return (<div>
-        <div className="grid md:grid-cols-2 grid-cols-1 gap-40 items-end mt-20">
-            <div>
-                <Title title="Providing The World's Best User Experiences" />
-                <p className="text-typography-flint text-lg mt-4">
-                Vestibulum blandit ut nunc vel bibendum semper elit eget dignissim rhoncus, nisl augue lacinia urna vel rutrum nisi nunc sit amet risus nulla ornare egestas.
-                </p>
-                <ul className="text-typography-flint list-image-checkmark">
-                    {items.map((item)=> <li key={item} className="mt-4 flex items-center gap-2">
-                        
-                        {item}
-    
-                    </li>)}
-                </ul>
+    return (
+        <div>
+            <div className="grid md:grid-cols-2 grid-cols-1 md:gap-20 mt-20 items-center">
+                <div className="xl:pr-20">
+                    <Title title="Providing The World's Best User Experiences" />
+                    <p className="text-typography-flint md:text-lg mt-4">
+                        Vestibulum blandit ut nunc vel bibendum semper elit eget dignissim rhoncus, nisl augue lacinia urna vel rutrum nisi nunc sit amet risus nulla ornare egestas.
+                    </p>
+                    <ul className="text-typography-flint items-center mt-4">
+                        {items.map((item) => <li key={item} className="flex items-center gap-2 mt-4 md:text-lg">
+                            <img src={check} alt="check" className="w-8"/>
+                           <span className="block">
+                           {item}
+                           </span>
+                        </li>)}
+                    </ul>
+                </div>
+                <div className="relative">
+                    <img src={img} alt="User Experience" className="z-10 relative"/>
+                    <img src={bgImg} alt="User Experience" className="md:absolute hidden bottom-20 z-0 w-2/3 right-0"/>
+                </div>
             </div>
-        </div>
-    </div>);
+        </div>);
 }
 
 export default CardBestUserExperience;
