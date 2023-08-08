81fec749dfba75f9d139525c9b4e8502d2d0bee4
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-08-08 20:55:38 +0330

    Subject: Footer, Description: Create new component for footer, Date: 8/8/2023

---
+++ src/layout/footer.tsx
@@ -0,0 +1,41 @@
+import logo from 'Images/logo.svg'
+import { Link } from 'react-router-dom';
+
+import facebook from 'Images/icons/facebook.svg'
+import twitter from 'Images/icons/twitter.svg'
+import dribbble from 'Images/icons/dribbble.svg'
+import linkedin from 'Images/icons/linkedin.svg'
+import ve from 'Images/icons/ve.svg'
+
+
+const Footer = () => {
+    const links = [
+        "Home",
+        "Abput us",
+        "Portfolio",
+        "contact Me",
+        "privacy Policy"
+    ]
+    
+    const socialMedias = [
+        facebook, twitter, dribbble, linkedin, ve
+    ]
+    return (<div className=" bg-background-graphite p-10 md:mt-20 mt-10">
+        <div className="lg:container xl:px-40 px-0 mx-auto  w-full">
+
+            <div className="logo flex items-center justify-center gap-2 mx-auto">
+                <img src={logo} alt='logo' className='w-4' />
+                <span className='font-bold text-white text-md:2xl text-xl'>DevAgency</span>
+            </div>
+            <div className='flex flex-wrap gap-8 justify-center my-10 pb-10 border-b border-b-[#A39189] text-[#D7D7D7]'>
+                {links.map((link) => <Link key={link} to={link} className=' whitespace-nowrap'>{link}</Link>)}
+            </div>
+
+            <div className='flex md:justify-end justify-center gap-4'>
+                {socialMedias.map((media)=> <img src={media} alt='social' />)}
+            </div>
+        </div>
+    </div>);
+}
+
+export default Footer;
