fa32c17ed3dfcaac2d4326f3c09ad7da09ac492b
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-08-09 21:59:39 +0330

    Subject: Slider, Description: Edit style for responsive view, Date: 8/9/2023

--- src/components/title/title.tsx
+++ src/components/title/title.tsx
@@ -3,7 +3,7 @@ type TitleProps = {
     customClass?: string
 }
 const Title = ({ title, customClass }: TitleProps) => {
-    return (<h3 className={`w-fit relative font-bold md:text-4xl text-2xl after:h-2 after:bg-background-maize after:w-full ${customClass} after:block after:absolute after:bottom-2 after:-z-1 `}>
+    return (<h3 className={`w-fit relative font-bold md:text-4xl text-2xl after:h-2 after:bg-background-maize  ${customClass ? customClass : "after:w-full"} after:block after:absolute after:bottom-2 after:-z-1 `}>
         {title}
     </h3>);
 }
