85bf701cf583548c4e1bc9ba23e1c40e76a2240d
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-08-09 21:04:13 +0330

    Subject: Style, Description: Add customClass props, Date: 8/9/2023

--- src/components/title/title.tsx
+++ src/components/title/title.tsx
@@ -1,10 +1,11 @@
 type TitleProps = {
     title: StringConstructor
+    customClass?: string
 }
-const Title = ({title}: TitleProps) => {
-    return ( <h3 className="w-fit relative font-bold md:text-4xl text-2xl after:h-2 after:bg-background-maize after:w-full after:block after:absolute after:bottom-2 after:-z-1">
+const Title = ({ title, customClass }: TitleProps) => {
+    return (<h3 className={`w-fit relative font-bold md:text-4xl text-2xl after:h-2 after:bg-background-maize after:w-full ${customClass} after:block after:absolute after:bottom-2 after:-z-1 `}>
         {title}
-    </h3> );
+    </h3>);
 }
- 
+
 export default Title;
