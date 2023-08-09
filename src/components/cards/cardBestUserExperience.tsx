75de5001b6a9e8bae5fcfc6a4e699356a54ee2ed
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-08-09 21:05:00 +0330

    Subject: Style, Description: Edit style for responsive view, Date: 8/9/2023

--- src/components/cards/cardBestUserExperience.tsx
+++ src/components/cards/cardBestUserExperience.tsx
@@ -11,22 +11,24 @@ const CardBestUserExperience = () => {
         <div>
             <div className="grid md:grid-cols-2 grid-cols-1 md:gap-20 md:mt-10 mt-5 items-center">
                 <div className="xl:pr-20">
-                    <Title title="Providing The World's Best User Experiences" />
+                    <Title title="Providing The World's Best User Experiences"
+                        customClass="xl:after:w-[20rem] md:after:w-[24rem] after:w-[13rem]"
+                    />
                     <p className="text-typography-flint md:text-lg mt-4">
                         Vestibulum blandit ut nunc vel bibendum semper elit eget dignissim rhoncus, nisl augue lacinia urna vel rutrum nisi nunc sit amet risus nulla ornare egestas.
                     </p>
                     <ul className="text-typography-flint items-center mt-4">
                         {items.map((item) => <li key={item} className="flex items-center gap-2 mt-4 md:text-lg">
-                            <img src={check} alt="check" className="w-8"/>
-                           <span className="block">
-                           {item}
-                           </span>
+                            <img src={check} alt="check" className="w-8" />
+                            <span className="block">
+                                {item}
+                            </span>
                         </li>)}
                     </ul>
                 </div>
                 <div className="relative">
-                    <img src={img} alt="User Experience" className="z-10 relative"/>
-                    <img src={bgImg} alt="User Experience" className="md:absolute md:block hidden bottom-20 z-0 w-2/3 right-0"/>
+                    <img src={img} alt="User Experience" className="z-10 relative" />
+                    <img src={bgImg} alt="User Experience" className="md:absolute md:block hidden bottom-20 z-0 w-2/3 right-0" />
                 </div>
             </div>
         </div>);
