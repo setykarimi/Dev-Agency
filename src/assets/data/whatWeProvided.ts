35ee087051ec56efa9510f319decc5e3b685c438
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-08-10 11:42:52 +0330

    Subject: Data, Description: Create new folder for static data, Date: 8/10/2023

---
+++ src/assets/data/whatWeProvided.ts
@@ -0,0 +1,47 @@
+import appDesign from "Images/cards/appDesign.svg";
+import webDesign from "Images/cards/webDesign.svg";
+import webDevelopment from "Images/cards/webDevelopment.svg";
+import interfaceDesign from "Images/cards/interfaceDesign.svg";
+import visualDesign from "Images/cards/visualDesign.svg";
+import uxDesign from "Images/cards/uxDesign.svg";
+
+export const whatWeProvided = [
+    {
+        img: appDesign,
+        title: "App Design",
+        desc: "Praesent placerated egestas egestas cras rhone magna rutrum tellus laoreet",
+        background: "#E3F8FA"
+    },
+    {
+        img: webDesign,
+        title: "Web Design",
+        desc: "Praesent placerated egestas egestas cras rhone magna rutrum tellus laoreet",
+        background: "#F4ECE0"
+    },
+    {
+        img: webDevelopment,
+        title: "Web Development",
+        desc: "Praesent placerated egestas egestas cras rhone magna rutrum tellus laoreet",
+        background: "#F5E6FE"
+    },
+    {
+        
+        img: interfaceDesign,
+        title: "Interface Design",
+        desc: "Praesent placerated egestas egestas cras rhone magna rutrum tellus laoreet",
+        background: "#FFE6E2"
+    },
+    {
+        img: visualDesign,
+        title: "Visual Design",
+        desc: "Praesent placerated egestas egestas cras rhone magna rutrum tellus laoreet",
+        background: "#E0E6F4"
+    },
+    {
+        img: uxDesign,
+        title: "Ux Design",
+        desc: "Praesent placerated egestas egestas cras rhone magna rutrum tellus laoreet",
+        background: "#E2FAE3"
+    }
+    
+]
