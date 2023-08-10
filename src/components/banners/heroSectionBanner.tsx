5e85d366584f4e24c120a18fc2bb6af82df616d4
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-08-10 10:27:42 +0330

    Subject: HeroSectionBanner, Description: Add content to banner, Date: 8/10/2023

--- src/components/banners/heroSectionBanner.tsx
+++ src/components/banners/heroSectionBanner.tsx
@@ -1,9 +1,27 @@
-const HeroSectionBanner = () => {
-    return ( 
-        <div>
-            HeroSectionBanner
+import chevronRight from "Images/icons/chevronRight.svg";
+import heroBannerBg from "Images/heroBannerBg.png";
+import { Link } from "react-router-dom";
 
+type BannerTypes = {
+    title: string
+}
+
+const HeroSectionBanner = ({title}: BannerTypes) => {
+    return ( 
+        <div className="bg-background-merino p-4">
+        <div className="lg:container mx-auto xl:px-40">
+            <div className="flex flex-col items-center justify-center  md:py-28 py-16" style={{background: `url(${heroBannerBg})`, backgroundRepeat: 'no-repeat', backgroundSize: "cover", backgroundPosition: 'center'}}>
+                <h1 className="md:text-5xl text-3xl font-extra-bold  md:mt-8 mt-4 md:px-0 px-10">
+                   {title}
+                </h1>
+                <div className="mt-6 flex items-center gap-2 text-typography-flint">
+                    <Link to="/">Home</Link>
+                    <span><img src={chevronRight} alt="right" /></span>
+                    <span>{title}</span>
+                </div>
+            </div>
         </div>
+    </div>
      );
 }
  
