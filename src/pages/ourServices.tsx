e7036f60262c07f435ea30a07c3d1b7ed3124e48
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-08-10 10:12:21 +0330

    Subject: Services, Description: Create new component for Services, Date: 8/10/2023

---
+++ src/pages/ourServices.tsx
@@ -0,0 +1,14 @@
+import HeroSectionBanner from "Components/banners/heroSectionBanner";
+
+const OurServices = () => {
+    return (
+        <>
+            <HeroSectionBanner title="Services"/>
+            <div className="lg:container xl:px-40 px-4 mx-auto">
+                hi
+            </div>
+        </>
+    );
+}
+
+export default OurServices;
