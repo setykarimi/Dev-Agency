e77339d9cc61c97e09a6ace3225c9b4eced5ea28
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-08-12 20:45:56 +0330

    Subject: Projects, Description: Create new file for maping on datas, Date: 8/12/2023

---
+++ src/assets/data/projects.ts
@@ -0,0 +1,27 @@
+import projectsWatch from "Images/cards/projectsWatch.png";
+import projectsSEO from "Images/cards/projectsSEO.png";
+import projectMDCL from "Images/cards/projectMDCL.png";
+import projectsEducation from "Images/cards/projectsEducation.png";
+
+export const projects = [
+    {
+        backgroundColor: "#F5E6FE",
+        img: projectsWatch,
+        title: "Watch - Product Responsive"
+    },
+    {
+        backgroundColor: "#F4ECE0",
+        img: projectsSEO,
+        title: "SEO Agency - Creative"
+    },
+    {
+        backgroundColor: "#E1F7F9",
+        img: projectsEducation,
+        title: "E-Online - Education"
+    },
+    {
+        backgroundColor: "#E0E6F4",
+        img: projectMDCL,
+        title: "MDCL - Medical"
+    }
+]
