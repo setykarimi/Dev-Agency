6787d91d2d7faf6ffedc45142a8cfe664ad5596b
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-08-13 22:30:57 +0330

    Subject: projects, Description: Edit for new property, Date: 8/13/2023

--- src/components/cards/cardOurProjects.tsx
+++ src/components/cards/cardOurProjects.tsx
@@ -1,8 +1,7 @@
 import Title from "Components/title/title";
 
-import img1 from "Images/cards/projectsWatch.png";
-import img2 from "Images/cards/projectsSEO.png";
-import { projects } from "../../assets/data/projects";
+import { projects } from "Data/projects";
+import { Link } from "react-router-dom";
 
 export default function CardOurProjects() {
     return (
@@ -19,10 +18,7 @@ export default function CardOurProjects() {
                 <div className="flex md:justify-end justify-center md:order-none order-last ">
                     <button className="bg-background-merino text-primary-orange-default md:py-3 py-2 md:px-8 px-4 h-fit w-fit rounded font-medium">See More</button>
                 </div>
-
                 {projects.slice(0, 2).map(({ backgroundColor, img, title }) => <Card backgroundColor={backgroundColor} img={img} title={title} />)}
-
-
             </div>
         </>
     );
@@ -33,16 +29,17 @@ type CardProps = {
     backgroundColor: string
     img: string
     title: string
+    path?: string
 }
 
-export const Card = ({ backgroundColor, img, title }: CardProps) => {
+export const Card = ({ backgroundColor, img, title, path }: CardProps) => {
     return (
-        <div className="pt-12 px-4 rounded" style={{ background: backgroundColor }}>
+        <Link to={path} className="pt-12 px-4 rounded" style={{ background: backgroundColor }}>
             <div className="block text-2xl font-bold text-center">
                 <span className="block">{title}</span>
                 <span className="block">Landing Page</span>
             </div>
             <img src={img} alt="landing" className="mt-8 mx-auto" />
-        </div>
+        </Link>
     )
 }
