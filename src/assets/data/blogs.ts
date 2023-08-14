9f3434d7c482aa5d4e2d85bf1017520051316583
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-08-14 21:27:49 +0330

    Subject: Blog, Description: Create new folder for blogs data, Date: 8/14/2023

---
+++ src/assets/data/blogs.ts
@@ -0,0 +1,45 @@
+import design1 from 'Images/blogs/design1.png'
+import travel1 from 'Images/blogs/travel1.png'
+import fashion1 from 'Images/blogs/fashion1.png'
+import creative1 from 'Images/blogs/creative1.png'
+import design2 from 'Images/blogs/design2.png'
+import travel2 from 'Images/blogs/travel2.png'
+
+export const blogs = [
+    {
+        img: design1,
+        category: "DESIGN",
+        title: "Best Design Tips You Will Read This Year",
+        date: "24 August 2022"
+    },
+    {
+        img: travel1,
+        category: "TRAVEL",
+        title: "The loss is not all that of surprising things",
+        date: "24 August 2022"
+    },
+    {
+        img: fashion1,
+        category: "FASHION",
+        title: "Should Fixing Fashion Take 100 Steps?",
+        date: "24 August 2022"
+    },
+    {
+        img: creative1,
+        category: "CREATIVE",
+        title: "Top 6 Ways To Buy A Used Fashion",
+        date: "24 August 2022"
+    },
+    {
+        img: design2,
+        category: "DESIGN",
+        title: "Travel And Love Have 9 Things In Common",
+        date: "24 August 2022"
+    },
+    {
+        img: travel2,
+        category: "TRAVEL",
+        title: "Best Fashion Tips You Will Read This Year",
+        date: "24 August 2022"
+    }
+]
