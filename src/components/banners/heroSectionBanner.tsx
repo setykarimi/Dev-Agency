4764cf1d796242fe40a65a711bde4b6b20754288
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-08-15 21:27:02 +0330

    Subject: HeroSections, Description: Edit for text-overflow, Date: 8/15/2023

--- src/components/banners/heroSectionBanner.tsx
+++ src/components/banners/heroSectionBanner.tsx
@@ -12,7 +12,7 @@ const HeroSectionBanner = ({title, breadcrumbs}: BannerTypes) => {
         <div className="bg-background-merino p-4">
         <div className="lg:container mx-auto xl:px-40">
             <div className="flex flex-col items-center justify-center  md:py-28 py-16" style={{background: `url(${heroBannerBg})`, backgroundRepeat: 'no-repeat', backgroundSize: "cover", backgroundPosition: 'center'}}>
-                <h1 className="md:text-5xl text-3xl font-extra-bold  md:mt-8 mt-4 md:px-0 px-10">
+                <h1 className="md:text-5xl text-3xl font-extra-bold md:mt-8 mt-4  md:px-10 md:max-w-[40rem] text-center">
                    {title}
                 </h1>
                 <div className="mt-6 flex items-center gap-2 text-typography-flint">
@@ -20,7 +20,7 @@ const HeroSectionBanner = ({title, breadcrumbs}: BannerTypes) => {
                     <span><img src={chevronRight} alt="right" /></span>
                     {breadcrumbs && <>  <span>{breadcrumbs}</span>
                     <span><img src={chevronRight} alt="right" /></span></>}
-                    <span>{title}</span>
+                    <span className="truncate max-w-[11rem]">{title}</span>
                 </div>
             </div>
         </div>
