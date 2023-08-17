792bc72c7454ad947a64e414569125b4f4e907bf
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-08-17 12:16:59 +0330

    Subject: Contact us, Description: Create new component for Contact us, Date: 8/17/2023

---
+++ src/pages/contactUs.tsx
@@ -0,0 +1,57 @@
+import location from "Images/icons/location.svg";
+import phone from "Images/icons/phone.svg";
+import email from "Images/icons/email.svg";
+import HeroSectionBanner from "../components/banners/heroSectionBanner";
+import Message from "../components/forms/message";
+
+const ContactUs = () => {
+    const details = [
+        {
+            img: location,
+            title: "Our Main Office",
+            subTitle: "24 Sylhet, Bagbari 40",
+            desc: "Central Rd, Modina Tower"
+        },
+        {
+            img: phone,
+            title: "Direct Call Us On",
+            subTitle: "(224) 4466-7788",
+            desc: "(224) 4466-7789"
+        },
+        {
+            img: email,
+            title: "E-mail Us At",
+            subTitle: "design@gmail.com",
+            desc: "example@gmail.com"
+        },
+    ]
+    return (
+        <>
+            <HeroSectionBanner title="Contact" />
+            <div className="lg:container xl:px-40 px-4 mx-auto md:mt-20 mt-10">
+                <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 md:gap-8 gap-4 md:mt-20 mt-10">
+                    {details.map(({ img, title, subTitle, desc }) => <div className="flex gap-4 items-center bg-background-merino xl:p-8 p-6 rounded-md">
+                        <div className="bg-primary-orange-default rounded-full md:w-20 w-16 md:h-20 h-16 flex justify-center items-center md:p-7 p-5">
+                            <img src={img} alt="icon" />
+                        </div>
+                        <div className="flex flex-col">
+                            <span className="block font-bold text-typography-graphite text-xl">{title}</span>
+                            <div className="mt-1">
+                                <span className="block text-typography-hurricane text-sm">
+                                    {subTitle}
+                                </span>
+                                <span className="block text-typography-hurricane text-sm">
+                                    {desc}
+                                </span>
+                            </div>
+                        </div>
+                    </div>)}
+                </div>
+
+                <Message />
+            </div>
+        </>
+    );
+}
+
+export default ContactUs;
