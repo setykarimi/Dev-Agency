23329a9e1e97777a6975583aafdd978265ab1af0
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-08-17 11:14:24 +0330

    Subject: Blog, Description: Add link props, Date: 8/17/2023

--- src/components/cards/cardBlog.tsx
+++ src/components/cards/cardBlog.tsx
@@ -1,31 +1,25 @@
+import { Link } from "react-router-dom"
+
 type CardBlogProps = {
     img: string
     category?: 'DESIGN' | 'FASHION' | 'TRAVEL' | 'CREATIVE'
     title: string
     date: string
+    path: string
 }
 
 
-const CardBlog = ({ img, category, title, date }: CardBlogProps) => {
+const CardBlog = ({ img, category, title, date, path }: CardBlogProps) => {
     return (
-        <div className="rounded-md">
+        <Link to={path} className="rounded-md">
             <img src={img} alt={category} className="rounded-t-md w-full" />
             <div className="py-6 px-8 bg-background-merino rounded-b">
                 <span className="w-fit block px-4 py-2 text-primary-orange-default bg-background-maize text-sm mx-auto font-semibold rounded">{category}</span>
                 <span className="text-typography-graphite font-bold md:text-2xl text-xl text-center block mt-4">{title}</span>
                 <span className="block text-center text-typography-flint mt-4">{date}</span>
             </div>
-        </div>
+        </Link>
     );
 }
 
 export default CardBlog;
-
-
-type HeadingProps = {
-    text : string
-}
-
-export const Heading = (props : HeadingProps) => {
-    return <h2>{props.text}</h2>
-}
