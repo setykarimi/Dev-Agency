078af746faf014e00cf937a8c907413aabe6ea23
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-08-17 11:56:09 +0330

    Subject: About us, Description: Create new data asset for our team data, Date: 8/17/2023

---
+++ src/assets/data/ourTeam.ts
@@ -0,0 +1,22 @@
+import freddie from "Images/ourTeam/freddie.png";
+import cameron from "Images/ourTeam/cameron.png";
+import gabriel from "Images/ourTeam/gabriel.png";
+
+
+export const ourTeam = [
+    {
+        img:freddie,
+        name: "Freddie Stewart",
+        position: "Ui Designer"
+    },
+    {
+        img:cameron,
+        name: "Cameron Butcher",
+        position: "Ux Designer"
+    },
+    {
+        img:gabriel,
+        name: "Gabriel Woods",
+        position: "Web Designer"
+    }
+]
