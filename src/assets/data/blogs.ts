53b6e0b2c7a1d0f670b09e8ff3cca30c8ca5a0bf
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-08-17 11:14:15 +0330

    Subject: Blog, Description: Add link props, Date: 8/17/2023

--- src/assets/data/blogs.ts
+++ src/assets/data/blogs.ts
@@ -10,36 +10,42 @@ export const blogs = [
         img: design1,
         category: "DESIGN",
         title: "Best Design Tips You Will Read This Year",
-        date: "24 August 2022"
+        date: "24 August 2022",
+        path: "/blog/best-design-tips"
     },
     {
         img: travel1,
         category: "TRAVEL",
         title: "The loss is not all that of surprising things",
-        date: "24 August 2022"
+        date: "24 August 2022",
+        path: ""
     },
     {
         img: fashion1,
         category: "FASHION",
         title: "Should Fixing Fashion Take 100 Steps?",
-        date: "24 August 2022"
+        date: "24 August 2022",
+        path: ""
     },
     {
         img: creative1,
         category: "CREATIVE",
         title: "Top 6 Ways To Buy A Used Fashion",
-        date: "24 August 2022"
+        date: "24 August 2022",
+        path: ""
     },
     {
         img: design2,
         category: "DESIGN",
         title: "Travel And Love Have 9 Things In Common",
-        date: "24 August 2022"
+        date: "24 August 2022",
+        path: ""
     },
     {
         img: travel2,
         category: "TRAVEL",
         title: "Best Fashion Tips You Will Read This Year",
-        date: "24 August 2022"
+        date: "24 August 2022",
+        path: ""
     }
 ]
