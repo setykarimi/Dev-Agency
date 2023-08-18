2cf6093f400fc652f27848715a58ee0b69756754
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-08-18 13:06:17 +0330

    Subject: Style, Description: Edit style for responsive view, Date: 8/18/2023

--- src/layout/navbar.tsx
+++ src/layout/navbar.tsx
@@ -1,7 +1,13 @@
 import logo from 'Images/logo.svg'
+import menu from 'Images/icons/menu.svg'
 import { Link } from 'react-router-dom';
+import { useState } from 'react'
+
+
 
 const Navbar = () => {
+
+    const [showMenu, setShowMenu] = useState<boolean>(false)
     const menuItems = [
         {
             name: "Home",
@@ -24,6 +30,11 @@ const Navbar = () => {
             link: "/about-us"
         }
     ]
+
+    const showMenuHandler = () => {
+        console.log(showMenu);
+        setShowMenu(!showMenu)
+    }
     return (
         <nav className="bg-background-merino p-4 flex sticky top-0 z-20">
             <div className="lg:container xl:px-40 px-0 mx-auto flex justify-between items-center w-full">
@@ -31,11 +42,15 @@ const Navbar = () => {
                     <img src={logo} alt='logo' className='w-4' />
                     <span className='font-bold text-typography-asphalt text-xl'>DevAgency</span>
                 </div>
-                <div className='md:flex hidden gap-8 items-center'>
-                    <div className='flex gap-4'>
+
+                <button className='md:hidden block' onClick={showMenuHandler}>
+                    <img src={menu} alt="menu" />
+                </button>
+                <div className={`flex md:flex-row flex-col md:static fixed md:top-auto top-0 md:p-0 p-8 md:left-auto md:h-auto ${showMenu ? "left-0" : "-left-96"} transition-all h-screen md:bg-transparent bg-background-maize md:w-auto w-1/2 gap-8 md:items-center`}>
+                    <div className={`flex md:flex-row flex-col gap-4 z-50`}>
                         {menuItems.map(({ name, link }) => <Link to={link} className='font-medium border-b border-b-transparent text-typography-flint hover:border-b hover:border-b-primary-orange-default hover:font-bold transition-all'>{name}</Link>)}
                     </div>
-                    <Link to="contact-us"className='py-2 px-4 bg-primary-orange-default text-white rounded'>Contact</Link>
+                    <Link to="contact-us" className='py-2 px-4 bg-primary-orange-default text-white rounded'>Contact</Link>
                 </div>
             </div>
         </nav>
