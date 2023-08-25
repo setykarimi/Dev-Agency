a2f6ff19e65336b26ccb5707460e297944f184f6
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-08-25 12:54:12 +0330

    Subject: Route, Description: Add scrollToTop function on changing route, Date: 8/25/2023

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,8 +1,13 @@
 import Navbar from "Layout/navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "Layout/footer";
+import { useEffect } from "react";
 
 const Layout = () => {
+    const { pathname } = useLocation();
+    useEffect(() => {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+      }, [pathname]);
     return ( 
         <div>
             <Navbar />
