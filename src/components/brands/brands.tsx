94fa7e528d9d9394185e4d89f819a74b5043ae92
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-08-31 15:32:01 +0330

    Subject: Debug, Description: Edit for passing props, Date: 8/31/2023

--- src/components/brands/brands.tsx
+++ src/components/brands/brands.tsx
@@ -3,14 +3,17 @@ import cocacola from "Images/logos/cocacola.svg"
 import google from "Images/logos/google.svg"
 import dribbble from "Images/logos/dribbble.svg"
 import amazon from "Images/logos/amazon.svg"
+import { forwardRef } from "react"
 
-const Brands = ({bottomRef}) => {
+
+const Brands = forwardRef<HTMLDivElement>(function (props, ref) {
+
     const images = [behance, cocacola, google, dribbble, amazon]
     return (
-        <div ref={bottomRef} className="flex flex-wrap md:justify-between justify-center items-center md:gap-4 gap-8 md:mt-20 mt-10">
-            {images.map((img) => <img src={img} alt="logo" className="lg:w-auto md:w-30 w-24"/>)}
+        <div ref={ref} className="flex flex-wrap md:justify-between justify-center items-center md:gap-4 gap-8 md:mt-20 mt-10">
+            {images.map((img) => <img src={img} alt="logo" className="lg:w-auto md:w-30 w-24" />)}
         </div>
     );
-}
+})
 
 export default Brands;
